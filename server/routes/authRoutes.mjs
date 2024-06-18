498715fed7c9f82216768b9bbfd3e252426f775e
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-06-19 00:35:00 +0200

    add middlewares

---
+++ server/routes/authRoutes.mjs
@@ -0,0 +1,36 @@
+import express from 'express';
+import User from '../models/User.mjs';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const router = express.Router();
+
+// Register a new user
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
--- server/routes/userRoutes.mjs
+++ server/routes/userRoutes.mjs
@@ -1,8 +1,19 @@
 // server/routes/userRoutes.js
 import express from 'express';
 import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../controllers/userController.mjs';
+import User from '../models/User.mjs';
+import protect from '../middlewares/authMiddleware.mjs';
 
 const router = express.Router();
+
+router.get('/', protect, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 router.route('/')
   .get(getUsers)
