7205c71ae9f1af9dfc9c61efc1060e4b1fb8c19d
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-07-02 00:50:59 +0200

    Ajout des routes CRUD pour les données globales dans configRoutes.js

--- server/routes/configRoutes.js
+++ server/routes/configRoutes.js
@@ -1,5 +1,19 @@
 import express from 'express';
 
+import {
+  createGlobalData,
+  getGlobalData,
+  getGlobalDataById,
+  updateGlobalData,
+  deleteGlobalData
+} from '../controllers/globalDataController.js';
+
+router.post('/', createGlobalData);
+router.get('/', getGlobalData);
+router.get('/:id', getGlobalDataById);
+router.put('/:id', updateGlobalData);
+router.delete('/:id', deleteGlobalData);
+
 const router = express.Router();
 
 // Ajoutez vos routes ici
