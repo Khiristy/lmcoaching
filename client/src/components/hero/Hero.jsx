70951de979567226b5b7917147b9a2b559172063
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-08-29 20:22:25 +0200

    add hero

---
+++ client/src/components/hero/Hero.jsx
@@ -0,0 +1,30 @@
+import "./Hero.module.scss";
+// import heroImg from "/media/hero_img.png";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="left_side-container">
+        <h1>
+          Achieve Your
+          <br /> Goals Faster
+        </h1>
+        <p>
+          Lorem Ipsumis simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley
+        </p>
+        <a href="" className="main_btn">
+          Réserver maintenant !
+        </a>
+      </div>
+      <div className="right_side-container">
+        {/* <div className="divider_top" />
+        <div className="divider_bottom" /> */}
+        {/* <img src={heroImg} alt="" className="hero_img" /> */}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import styles from "./Home.module.scss";
+import "./Home.module.scss";
+import Hero from "/src/components/hero/Hero.jsx";
+
 
 const Home = () => (
-
-    <div className={styles.home}>
+  
+    <div className="home">
+      <Hero />
       <h1>Welcome to lmcoaching</h1>
     </div>
 
 );
-
 export default Home;
