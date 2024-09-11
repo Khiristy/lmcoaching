a02b4400708e17eb7133e434470670b0a1252bba
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-09-11 05:37:02 +0200

    update faq

--- client/src/features/Home/Faq/HomeFaq.jsx
+++ client/src/features/Home/Faq/HomeFaq.jsx
@@ -1,18 +1,21 @@
 import "./HomeFaq.scss";
 import CollapsibleFaq from "../../Shared/FaqComponents/CollapsibleFaq.jsx";
+import faqImg from "/media/faq_img.webp";
 
 const FaqSection = () => {
   return (
-    <section className="faq_section">
-      <div className="faq_section-title">
-        <h3>FAQ</h3>
-      </div>
-      <div className="faq_section-collapsible">
-        <CollapsibleFaq />
-        <CollapsibleFaq />
-        <CollapsibleFaq />
-        <CollapsibleFaq />
-        <CollapsibleFaq />
+    <section className="faq">
+      <h3>FAQ</h3>
+      <div className="faq_content">
+        <img src={faqImg} alt="" className="faq_content-img" />
+
+        <div className="faq_content-collapsible">
+          <CollapsibleFaq />
+          <CollapsibleFaq />
+          <CollapsibleFaq />
+          <CollapsibleFaq />
+          <CollapsibleFaq />
+        </div>
       </div>
     </section>
   );
