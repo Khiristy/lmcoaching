6ab56185b270a6fd0f2944aa5796b3afce017a89
Khiristy <147670388+Khiristy@users.noreply.github.com>
2024-10-19 22:02:00 +0200

    update form

--- client/src/pages/Form/Form.jsx
+++ client/src/pages/Form/Form.jsx
@@ -1,15 +1,13 @@
 import "./Form.scss";
 import MainForm from "../../features/Form/MainForm/MainForm.jsx";
-import bckImg from "/media/form/bck_img-3.png"
+import bckImg from "/media/form/bck_img-3.png";
 const Form = () => {
-    
-    return (
-      <div className="form">
-        <img src={bckImg} alt="" className="bckImg"/>
-          <MainForm />
-      </div>
-    );
-  };
-  
-  export default Form;
-  
+  return (
+    <div className="form">
+      <img src={bckImg} alt="" className="bckImg" />
+      <MainForm />
+    </div>
+  );
+};
+
+export default Form;
